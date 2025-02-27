0dfd2780a1d41b83a48d6346861daca8fca2dfc9
lxtz0109 <wangrander@163.com>
2025-02-27 12:28:26 +0800

    RabbitMq实例

--- src/app/Api/Lakers.php
+++ src/app/Api/Lakers.php
@@ -5,6 +5,9 @@ namespace App\Api;
 use PhalApi\Api;
 use App\Domain\Examples\CURD as DomainCURD;
 use PhalApi\Response;
+use PhpAmqpLib\Connection\AMQPStreamConnection;
+use PhpAmqpLib\Message\AMQPMessage;
+
 
 /**
  * 湖人家庭
@@ -26,6 +29,95 @@ class Lakers extends Api {
         //\PhalApi\DI()->response->setMsg('手动设置提示消息');
 		//return array('content' => '中国','data' => $listItems);
 	}
+
+
+    /**
+     * 湖人队队列--生产者
+     * @desc这是一个测试湖人队列生产者的接口
+     */
+    public function rabbitMqProduce() {
+        for($i = 0; $i < 1000; $i++) {
+            // 创建连接到 RabbitMQ 服务器
+            $connection = new AMQPStreamConnection('localhost', 5672, 'guest', 'guest');
+            $channel = $connection->channel();
+
+
+            // 声明队列
+            $channel->queue_declare('task_queue', false, true, false, false);
+
+            // 发送的消息内容
+            $data = "Hello!======".$i;
+            $msg = new AMQPMessage($data, array('delivery_mode' => 2)); // 设置消息为持久化
+            //$msg = new AMQPMessage($data, array('delivery_mode' => AMQPMessage::DELIVERY_MODE_PERSISTENT));
+
+            // 发布消息到队列
+            $channel->basic_publish($msg, '', 'task_queue');
+
+            echo " [x] Sent ".$data."\n";
+
+            // 关闭连接
+            $channel->close();
+
+        }
+
+
+        $domainCurd = new DomainCURD();
+        $listItems = $domainCurd->getList(1,1,10);
+
+        return $listItems;
+        //设置状态码
+        //\PhalApi\DI()->response->setRet(1000);
+        //手动设置提示消息
+        //\PhalApi\DI()->response->setMsg('手动设置提示消息');
+        //return array('content' => '中国','data' => $listItems);
+    }
+
+    /**
+     * 湖人队队列--消费者
+     * @desc这是一个测试湖人队列消费者的接口
+     */
+    public function rabbitMqConsumer() {
+        for($i = 0; $i < 1000; $i++) {
+
+
+        // 创建连接到 RabbitMQ 服务器
+        $connection = new AMQPStreamConnection('localhost', 5672, 'guest', 'guest');
+        $channel = $connection->channel();
+
+        // 声明队列
+        $channel->queue_declare('task_queue', false, true, false, false);
+
+        echo " [*] Waiting for messages. To exit press CTRL+C\n";
+
+         // 回调函数，用于处理接收到的消息
+        $callback = function ($msg) {
+            echo ' [x] Received ', $msg->body, "\n";
+            // 模拟任务处理
+            sleep(substr_count($msg->body, '.'));
+            echo " [x] Done\n";
+            // 手动确认消息已经处理
+           //$msg->ack();
+            $msg->delivery_info['channel']->basic_ack($msg->delivery_info['delivery_tag']);
+        };
+
+        // 告诉 RabbitMQ 在同一时间不要发送多于一条消息给一个消费者
+        $channel->basic_qos(null, 1, null);
+
+        // 告诉 RabbitMQ 使用回调函数来接收消息，并手动确认消息
+        $channel->basic_consume('task_queue', '', false, false, false, false, $callback);
+
+        // 等待消息进入队列
+       // while ($channel->is_consuming()) {
+            $channel->wait();
+        //}
+
+        // 关闭连接
+        $channel->close();
+        $connection->close();
+
+        }
+    }
+
 
 
 }
---
+++ src/app/Api/Rabbitmq/Testdemo1.php
@@ -0,0 +1,117 @@
+<?php
+namespace App\Api\Rabbitmq;
+
+use PhalApi\Api;
+use PhpAmqpLib\Connection\AMQPStreamConnection;
+use PhpAmqpLib\Message\AMQPMessage;
+
+/**
+ * rabbitmq案例
+ * @author wzh 20250227
+ */
+
+class Testdemo1 extends Api {
+    public function getRules() {
+        return array(
+            // 字符串
+            'str' => array(
+                'str' => array('name' => 'str', 'desc' => '简单的字符串参数'),
+            ),
+            // 生产者
+            'producer' => array(
+                'senddata' => array('name' => 'senddata', 'desc' => '生产者发送内容'),
+            ),
+            // 消费者
+            'comsumer' => array(
+
+            ),
+        );
+    }
+    /** ---------------------- string 字符串参数 ---------------------- **/
+
+    /**
+     * 参数示例 - 字符串参数
+     * @desc 简单的字符串参数传递案例
+     */
+    public function str() {
+        return $this->str();
+    }
+
+    /** ----------------------生产者接口---------------------- **/
+
+    /**
+     * rabbitmq生产者接口
+     * @desc 这是一个rabbitmq生产者案例
+     */
+    public function producer() {
+        $senddata = $this->senddata;
+        $connect = new AMQPStreamConnection('localhost', 5672, 'guest', 'guest');
+        $channel = $connect->channel();
+
+        // 第三参数持久化
+        $channel->queue_declare('swoft_queue_test', false,true,false,false);
+        # 消息
+        $msg = new AMQPMessage('swoft_queue_test'.time(),array('content_type' => 'text/plain', 'delivery_mode' => 2));
+
+        $channel->confirm_select(); // 发布确认模式
+
+
+        //推送成功
+         $channel->set_ack_handler(
+             function (AMQPMessage $message) {
+
+                 echo "发送成功: " . $message->body . PHP_EOL;exit;
+             }
+         );
+
+          //推送失败
+        /*   $channel->set_nack_handler(
+              function (AMQPMessage $message) {
+                  echo "发送失败: " . $message->body . PHP_EOL;
+              }
+          );*/
+        # 发送
+        $channel->basic_publish($msg,'','swoft_queue_test');
+        $channel->wait_for_pending_acks();
+
+        $channel->close();
+        $connect->close();
+        return $senddata;
+    }
+
+    /** ----------------------消费者接口---------------------- **/
+
+    /**
+     * rabbitmq消费者接口
+     * @desc 这是一个rabbitmq消费者案例
+     */
+    public function comsumer() {
+
+        $connect = new AMQPStreamConnection('localhost', 5672, 'guest', 'guest');
+        $channel = $connect->channel();
+        $channel->queue_declare('swoft_queue_test', false,true,false,false,false);
+
+        # 回调
+        $callback = function ($msg){
+          //  echo $msg->body.PHP_EOL;
+          //  echo "消费====\n";
+            // 手动应答
+            $msg->delivery_info['channel']->basic_ack($msg->delivery_info['delivery_tag']);
+        };
+
+        // 公平调度
+        $channel->basic_qos(null, 1, null);
+        // 第四参数为自动应答, 设置为false
+        $channel->basic_consume('swoft_queue_test','',false,false,false,false,$callback);
+        //while(count($channel->callbacks)){
+            $channel->wait();
+        //}
+
+
+        $channel->close();
+
+        return "ok";
+    }
+
+
+}
